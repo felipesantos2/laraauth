96a84d4c6bc2601d6cbfff2d96151064632df9da
felipesantos2 <santospinheiro627@gmail.com>
2024-09-27 09:04:04 -0300

    Organizing view folders

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -6,7 +6,7 @@
 
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <h1 class="text-2xl">
-                Bem-vindo ...
+                Bem-vindo
         </h1>
     </div>
 
---
+++ resources/views/guest/pages/login.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.form')
+
+@section('name', 'Login')
+
+@section('content')
+
+
+<form action="" method="post">
+
+Organizing view folders
+
+</form>
+
+
+@endsection
